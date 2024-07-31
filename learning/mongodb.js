6e43be02d3dc3f681723c9635d1f28a18f93631a
matthew Wong <matthew.wong128@gmail.com>
2024-07-31 12:01:12 -0400

    changed to async

--- learning/mongodb.js
+++ learning/mongodb.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+//mongodb+srv://user:ltRvk6bH7SXYoler@cluster0.6dobhny.mongodb.net/
+
 if (process.argv.length < 5) {
   console.log('give password as argument');
   process.exit(1);
@@ -8,15 +10,19 @@ const password = process.argv[2];
 const name = process.argv[3];
 const number = process.argv[4];
 
-//mongodb+srv://user:ltRvk6bH7SXYoler@cluster0.6dobhny.mongodb.net/
-
 const url = `mongodb+srv://user:${password}@cluster0.6dobhny.mongodb.net/`;
 mongoose.set('strictQuery', false);
 
-mongoose
-  .connect(url)
-  .then((res) => console.log('connected ', res))
-  .catch((err) => console.log('FAILED ', err));
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(url);
+    console.log('connected ', res);
+  } catch (error) {
+    console.log('FAILED ', error);
+  }
+};
+
+connectToDatabase();
 
 const personSchema = new Schema({
   name: String,
