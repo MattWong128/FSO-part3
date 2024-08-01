b0f1e13b9f4b6d578c6357d14cfb768b823f6d0e
matthew Wong <matthew.wong128@gmail.com>
2024-08-01 12:20:32 -0400

    post works

--- learning/index.js
+++ learning/index.js
@@ -44,10 +44,8 @@ app.get('/api/notes', (request, response) => {
     response.json(notes);
   });
 });
-
+//To do
 app.get('/api/notes/:id', (request, response) => {
-  // const id = request.params.id
-
   Note.findById(request.params.id)
     .then((note) => response.json(note))
     .catch((err) => {
@@ -55,6 +53,7 @@ app.get('/api/notes/:id', (request, response) => {
       // Error occurred during the query
       response.status(500).json({
         error: 'No note exists with that ID',
+        response: err,
       });
     });
 });
@@ -67,28 +66,21 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end('deleted succesfully');
 });
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
+
 app.post('/api/notes', (request, response) => {
-  console.log('hello');
   const body = request.body;
+
   if (!body.content) {
     return response.status(400).json({
       error: 'content missing',
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
-
-  notes = notes.concat(note);
-
-  response.json(note);
+  });
+  note.save().then((savedNote) => response.json(savedNote));
 });
 
 app.use((request, response) => {
